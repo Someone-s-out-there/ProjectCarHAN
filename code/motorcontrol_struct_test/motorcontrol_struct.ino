611e87f0b35e79628ce2b2976f0e6e5f49b2ea58
GhostJulian <99392239+GhostJulian@users.noreply.github.com>
2023-09-29 12:45:42 +0200

    testcode struct
    INCOMPLEET

---
+++ code/motorcontrol_struct_test/motorcontrol_struct.ino
@@ -0,0 +1,89 @@
+// van motordiver LN298N
+#define IN1 8   //11
+#define IN3 11  //3
+
+#define EN1 9
+#define EN2 10
+
+class MOTORCONTROL {
+public:
+  void MOTOR(uint8_t EN1, uint8_t IN1, uint8_t IN3, uint8_t EN2) {
+    EN_Motor_A = EN1;
+
+    IN1_Motor_A = IN1;
+    IN3_Motor_B = IN3;
+
+    EN_Motor_B = EN2;
+
+    // motor links
+    pinMode(EN_Motor_A, OUTPUT);
+    pinMode(IN1_Motor_A, OUTPUT);
+
+    // motor rechts
+    pinMode(EN_Motor_B, OUTPUT);
+    pinMode(IN3_Motor_B, OUTPUT);
+  }
+void setSpeed(int snelheid)
+{
+  motorSpeed = snelheid;
+}
+
+void forward()
+{
+  analogWrite(EN_Motor_A, motorSpeed); // PWM
+  digitalWrite(IN1_Motor_A, HIGH);
+
+  analogWrite(EN_Motor_B, motorSpeed); // PWM
+  digitalWrite(IN3_Motor_B, HIGH);
+}
+
+void left()
+{
+  analogWrite(EN_Motor_A, motorSpeed); // PWM
+  digitalWrite(IN1_Motor_A, HIGH);
+
+  analogWrite(EN_Motor_B, motorSpeed); // PWM
+  digitalWrite(IN3_Motor_B, LOW);
+}
+
+void right()
+{
+  analogWrite(EN_Motor_A, motorSpeed); // PWM
+  digitalWrite(IN1_Motor_A, LOW);
+
+  analogWrite(EN_Motor_B, motorSpeed); // PWM
+  digitalWrite(IN3_Motor_B, HIGH);
+}
+
+void stop()
+{
+  analogWrite(EN_Motor_A, 0); // pwm
+  digitalWrite(IN1_Motor_A, LOW);
+
+  analogWrite(EN_Motor_B,0); // pwm
+  digitalWrite(IN3_Motor_B, LOW);
+}
+
+void backward()
+{
+  analogWrite(EN_Motor_A, motorSpeed); // pwm
+  digitalWrite(IN1_Motor_A, LOW);
+
+  analogWrite(EN_Motor_B,motorSpeed); // pwm
+  digitalWrite(IN3_Motor_B, LOW);
+}
+
+private:
+  uint8_t EN_Motor_A, IN1_Motor_A, EN_Motor_B, IN3_Motor_B;
+  uint8_t motorSpeed = 70;
+}
+
+
+  MOTORCONTROL motor(EN1, IN1, IN3, EN2);
+setup() {
+  motor.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
