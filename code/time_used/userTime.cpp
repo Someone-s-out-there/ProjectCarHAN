7fb0216da9e668e7dffe8923c17a766b9df567ba
JACKAR710 <143492836+JACKAR710@users.noreply.github.com>
2023-10-09 16:39:20 +0200

    Update userTime.cpp
    Comments added

--- code/time_used/userTime.cpp
+++ code/time_used/userTime.cpp
@@ -14,20 +14,28 @@ bool displayTimeOnStartup = true;
 
 void getUserTime()
 {
+
+  // Starts reading bytes from byte 0
   while(1)
   {
+    // Reads the byte
     EEPROM.get(EEPROMByteNumber, EEPROMByteValue);
+
+    // Adds the value of the current byte to the total EEPROM value
     EEPROMValue += EEPROMByteValue;
 
+    // If the value of a byte is 255, EEPROMByteNumber is increased by one and the loop starts over
+    // If the value of a byte is not 255, the loop stops and the program is ready to write
     if (EEPROMByteValue == 255)
     {
       EEPROMByteNumber++;
-    }else {break;}
+    } else {break;}
   }
 }
 
 void updateUserTime()
 {
+  // Calculates and displays the time once on startup
   if (displayTimeOnStartup)
   {
     uint32_t hours = EEPROMValue / 360;
@@ -40,23 +48,27 @@ void updateUserTime()
 
   uint32_t currentMillis = millis();
 
-  // Happens after every 10000 milliseconds
+  // Happens after every <interval> milliseconds
   if (currentMillis - previousMillis >= interval)
   {
     // Resets timer to 0
     previousMillis = currentMillis;
     
+    // Total EEPROM value is increased by one
     EEPROMValue++;
 
+    // EEPROM current byte value is increased by one and is written to the EEPROM
     EEPROMByteValue++;
     EEPROM.update(EEPROMByteNumber, EEPROMByteValue);
 
+    // If the value of the current byte is 255 the program moves to the next byte starting from 0
     if (EEPROMByteValue == 255)
     {
       EEPROMByteNumber++;
       EEPROMByteValue = 0;
     }
 
+    // Calculates and displays the time
     uint32_t hours = EEPROMValue / 360;
     uint32_t minutes = (EEPROMValue % 360) / 6;
 
