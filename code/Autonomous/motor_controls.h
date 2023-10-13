beae9ce79a9d30a4f83d6c6fe9513485d83a17e4
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-10-13 11:09:16 +0200

    formatted

--- code/Autonomous/motor_controls.h
+++ code/Autonomous/motor_controls.h
@@ -2,9 +2,9 @@
   By Julian Janssen & Sjoerd van de Wege
   05-10-2023
 
-  Concept motorcontol with class MOTORCONTROL 
+  Concept motorcontol with class MOTORCONTROL
 
-  Tested on de L298N motordriver  
+  Tested on de L298N motordriver
   Use Serial monitor on 9600 baud
 */
 // #ifndef ARDUINO_H
@@ -16,7 +16,8 @@
 
 class MOTORCONTROL {
 public:
-  MOTORCONTROL(uint8_t enable1, uint8_t pwm2, uint8_t pwm3, uint8_t enable2, uint8_t l_led, uint8_t r_led);
+  MOTORCONTROL(uint8_t enable1, uint8_t pwm2, uint8_t pwm3, uint8_t enable2,
+               uint8_t l_led, uint8_t r_led);
   void setSpeed(uint8_t snelheid);
   void forward();
   void left();
@@ -24,7 +25,7 @@ public:
   void stop();
   void estop();
   void backward();
-  
+
 private:
   uint8_t EN_Motor_A, IN1_Motor_A, EN_Motor_B, IN3_Motor_B, left_Led, right_Led;
   uint8_t motorSpeed = 70;
