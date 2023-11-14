2033bb1807bd129b791102baca566390bcccfac1
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-11-14 12:19:48 +0100

    removed uart from wrong branch'

--- RobotCar/main.c
+++ RobotCar/main.c
@@ -6,27 +6,12 @@
  */ 
 
 #include <avr/io.h>
-#include <util/delay.h>
-#include "Uart/Uart.h"
-
-uint8_t Charakter=0;
+#include "IR_Sensors/IR.h"
 
 int main(void)
 {
-  uart_init();
-  DDRB |= (1<<DDB5);
-  uart_puts("fuck\n");
-  /* Replace with your application code */
   while (1) 
-	      {
-		//uart_puts("f");
-		if(Charakter !=0){
-		  uart_puts("f\n");
-		  Charakter = 0;
-		} else if (Charakter == 'a'){
-		  uart_puts("a\n");
-		  Charakter = 0;
-		}
-	      }
+	{}
 }
+
 
