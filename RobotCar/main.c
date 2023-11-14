656747ebd008eac1bd8c7ef728da1d7b5116ca47
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-11-14 11:33:38 +0100

    misc cmake changes and main to test uart

--- RobotCar/main.c
+++ RobotCar/main.c
@@ -6,13 +6,27 @@
  */ 
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include "Uart/Uart.h"
 
+uint8_t Charakter=0;
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+  uart_init();
+  DDRB |= (1<<DDB5);
+  uart_puts("fuck\n");
+  /* Replace with your application code */
+  while (1) 
+	      {
+		//uart_puts("f");
+		if(Charakter !=0){
+		  uart_puts("f\n");
+		  Charakter = 0;
+		} else if (Charakter == 'a'){
+		  uart_puts("a\n");
+		  Charakter = 0;
+		}
+	      }
 }
 
