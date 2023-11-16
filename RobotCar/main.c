5525d0d9a610029ba42a9059ed99e9d93e3fda38
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-11-16 11:32:50 +0100

    rebasw

--- RobotCar/main.c
+++ RobotCar/main.c
@@ -3,22 +3,16 @@
  *
  * Created: 10/23/2023 7:16:03 PM
  * Author : Sjoerd
- */ 
+ */
 
-#include <avr/io.h>
 #include "IR_Sensors/IR.h"
+#include <avr/io.h>
 
-int main(void)
-{
-<<<<<<< HEAD
-=======
+int main(void) {
   uart_init();
-  DDRB |= (1<<DDB5);
+  DDRB |= (1 << DDB5);
   uart_puts("test");
   /* Replace with your application code */
->>>>>>> 676c9c9 (removed fuck)
-  while (1) 
-	{}
+  while (1) {
+  }
 }
-
-
