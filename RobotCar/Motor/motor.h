e89b58e81c3ac18cc3d8797e63da183dff63228b
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-11-28 12:01:18 +0100

    added motor driver frame work, needs implementation

---
+++ RobotCar/Motor/motor.h
@@ -0,0 +1,16 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+typedef struct {
+  void (*setSpeed)(int);
+  void (*foward)(void);
+  void (*backward)(void);
+  void (*left)(void);
+  void (*right)(void);
+  void (*stop)(void);
+  void (*estop)(void);
+} MotorDRV;
+
+void motor_init(MotorDRV *m);
+
+#endif
