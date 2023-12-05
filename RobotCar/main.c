8838dd5b164ac1e26633d93229fc12b942a4f5f1
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-12-05 10:58:14 +0100

    fixed main.c merge debacle

--- RobotCar/main.c
+++ RobotCar/main.c
@@ -5,30 +5,10 @@
  * Author : Sjoerd
  */
 
-#include "Uart/Uart.h"
 #include <avr/io.h>
 #include <util/delay.h>
 
-uint8_t Charakter = 0;
-
 int main(void) {
-<<<<<<< HEAD
-=======
-  uart_init();
-  DDRB |= (1 << DDB5);
-  uart_puts("fuck\n");
-  /* Replace with your application code */
-  while (1) {
-    // uart_puts("f");
-    if (Charakter != 0) {
-      uart_puts("f\n");
-      Charakter = 0;
-    } else if (Charakter == 'a') {
-      uart_puts("a\n");
-      Charakter = 0;
+    while (1) {
     }
-  }
->>>>>>> 676c9c9 (removed fuck)
-  while (1) {
-  }
 }
