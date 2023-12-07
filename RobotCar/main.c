c6cb8ef157b4b8be45a67bf9f68d7c99fcda94be
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-12-07 11:12:58 +0100

    lcdDisplay voor joep

--- RobotCar/main.c
+++ RobotCar/main.c
@@ -8,9 +8,12 @@
 #include "Functions/Appinfo.h"
 #include "Functions/Buttons/mode_select.h"
 #include "Functions/millis/millis.h"
+#include "i2c/lcd.h"
 #include <avr/io.h>
 
 int main(void) {
+  lcd1602_init();
+  lcd1602_send_string("hello");
   IO_init();
   millis_init();
 
