3fc2c0c634d195e075def762f8f2c66425d41d3e
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2023-12-14 13:19:13 +0100

    [wip] main to test
    Signed-off-by: Sjoerd van de Wege <sjoerd.vandewege@gmail.com>

--- RobotCar/main.c
+++ RobotCar/main.c
@@ -8,23 +8,19 @@
 #include "Uart/Uart.h"
 #include <avr/io.h>
 #include <util/delay.h>
-
-uint8_t Charakter = 0;
+#include "ultrasonic/Ultrasonic.h"
+//uint8_t Charakter = 0;
 
 int main(void) {
   uart_init();
   DDRB |= (1 << DDB5);
-  uart_puts("fuck\n");
+    ultrasonic_init();
+
   /* Replace with your application code */
   while (1) {
-    // uart_puts("f");
-    if (Charakter != 0) {
-      uart_puts("f\n");
-      Charakter = 0;
-    } else if (Charakter == 'a') {
-      uart_puts("a\n");
-      Charakter = 0;
-    }
+      uint8_t tmp[2] = {0, 0};
+      tmp[0] = ultrasonic_get_distance();
+      uart_puts(tmp);
   }
   while (1) {
   }
