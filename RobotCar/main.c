769272105dae2e6351ca3a33118e939eb5ca13c5
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-19 13:17:00 +0100

    remotectrl works

--- RobotCar/main.c
+++ RobotCar/main.c
@@ -8,7 +8,16 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+#include "DriveModes/remote.h"
+#include "Motor/motor.h"
+#include "Uart/Uart.h"
+extern MotorDRV motor;
 int main(void) {
+    DDRB |= (1 << PORTB5);
+    uart_init();
+    motor_init(&motor);
+
     while (1) {
+        RemoteControl_Mode();
     }
 }
