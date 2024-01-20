d651724bf7858f3992704587fdb51970179fdace
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-20 14:05:29 +0100

    merged motor code and made main working

--- RobotCar/Motor/motor.c
+++ RobotCar/Motor/motor.c
@@ -6,7 +6,10 @@
 
 MotorDRV motor;
 
+static uint16_t currentspeed = 512;
+
 void motor_init(MotorDRV *m) {
+    // Link functions
     m->setSpeed = &setSpeed;
     m->foward = &foward;
     m->backward = &backward;
@@ -14,42 +17,105 @@ void motor_init(MotorDRV *m) {
     m->right = &right;
     m->stop = stop;
     m->estop = &estop;
+
+    // Configure PB0, PB1, PB2 and PB3 as output
+    DDRB |= (1 << PORTB3) | (1 << PORTB2) | (1 << PORTB1) | (1 << PORTB0);
+
+    /*
+    Set PB0 and PB3 LOW
+    PB 1 and PB2 will be controlled by  timer1
+    */
+    PORTB &= ~(1 << PORTB3) & ~(1 << PORTB0);
+    /*
+    | NOTE Timer 1 is 16 bits large!
+    |
+    |	Enable Fast-PWM mode 10 bits
+    | BOTTOM -> TOP = 0 -> 1023
+    | COM1A1 & COM1B1 = enable compare OC1A & OC1B
+    \*/
+    TCCR1A |= (1 << WGM12) | (1 << WGM11) | (1 << WGM10);
+    // TCCR1B |= (1<<CS11) | (1<<CS10);
+
+    /*
+    TOP = (F_CPU / (F_PWM * N)) - 1
+    = (16MHz / (1953.1z * 8)) - 1
+
+    prescaler 256 = 61.029hz
+    prescaler 64 = 244.2hz = 1023
+    prescaler 8 = 1953.1hz
+
+    Set the prescaler on 8, so the frequenty is about 1953 hz.
+    */
+    ICR1 = 1023;
+
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
+
+    /*
+    Set starting compare value
+    */
+    OCR1A = currentspeed;
+    OCR1B = currentspeed;
 }
 
-void setSpeed(int speed) { static uint8_t currentspeed; }
+void setSpeed(uint16_t speed) { currentspeed = speed; }
 
 void foward(void) {
-    PORTB |= (1 << PORTB5);
-    _delay_ms(100);
-    PORTB &= ~(1 << PORTB5);
+    // Set PB0 and PB3 to HIGH
+    PORTB |= (1 << PORTB3) | (1 << PORTB0);
+
+    OCR1A = currentspeed;
+    OCR1B = currentspeed;
+
+    // turn on the timer1
+    TCCR1B |= (1 << CS11) | (1 << CS10);
 }
 
 void backward(void) {
-    PORTB |= (1 << PORTB5);
-    _delay_ms(100);
-    PORTB &= ~(1 << PORTB5);
+    // Set PB0 and PB3 LOW
+    PORTB &= ~(1 << PORTB3) & ~(1 << PORTB0);
+
+    OCR1A = currentspeed;
+    OCR1B = currentspeed;
+
+    // turn on the timer1
+    TCCR1B |= (1 << CS11) | (1 << CS10);
 }
 
 void left(void) {
-    PORTB |= (1 << PORTB5);
-    _delay_ms(100);
-    PORTB &= ~(1 << PORTB5);
+    // Set PB0 to HIGH and PB3 to LOW
+    PORTB |= (1 << PORTB0);
+    PORTB &= ~(1 << PORTB3);
+
+    OCR1A = currentspeed;
+    OCR1B = currentspeed;
+
+    // turn on the timer1
+    TCCR1B |= (1 << CS11) | (1 << CS10);
 }
 
 void right(void) {
-    PORTB |= (1 << PORTB5);
-    _delay_ms(100);
-    PORTB &= ~(1 << PORTB5);
+    // Set PB0 to HIGH and PB3 to LOW
+    PORTB |= (1 << PORTB3);
+    PORTB &= ~(1 << PORTB0);
+
+    OCR1A = currentspeed;
+    OCR1B = currentspeed;
+
+    // turn on the timer1
+    TCCR1B |= (1 << CS11) | (1 << CS10);
 }
 
 void stop(void) {
-    PORTB |= (1 << PORTB5);
-    _delay_ms(100);
-    PORTB &= ~(1 << PORTB5);
+    OCR1A = 0;
+    OCR1B = 0;
+
+    // turn off the timer1
+    TCCR1B &= ~(1 << CS11) & ~(0 << CS10);
 }
 
 void estop(void) {
-    PORTB |= (1 << PORTB5);
-    _delay_ms(100);
-    PORTB &= ~(1 << PORTB5);
+    OCR1A = 0;
+    OCR1B = 0;
+    // turn off the timer1
+    TCCR1B &= ~(1 << CS11) & ~(1 << CS10);
 }
--- RobotCar/Motor/motor.h
+++ RobotCar/Motor/motor.h
@@ -1,19 +1,20 @@
 #ifndef MOTOR_H
 #define MOTOR_H
 
+#include <stdint.h>
 typedef struct {
-  void (*setSpeed)(int);
-  void (*foward)(void);
-  void (*backward)(void);
-  void (*left)(void);
-  void (*right)(void);
-  void (*stop)(void);
-  void (*estop)(void);
+    void (*setSpeed)(uint16_t);
+    void (*foward)(void);
+    void (*backward)(void);
+    void (*left)(void);
+    void (*right)(void);
+    void (*stop)(void);
+    void (*estop)(void);
 } MotorDRV;
 
 void motor_init(MotorDRV *m);
 
-void setSpeed(int speed);
+void setSpeed(uint16_t speed);
 
 void foward(void);
 
